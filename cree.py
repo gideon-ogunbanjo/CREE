8211f2e32533106994ace3444280f925d76c8473
Gideon-Ogunbanjo <85284242+gideon-ogunbanjo@users.noreply.github.com>
2023-04-26 23:00:17 +0100

    split the data into training and testing sets

--- cree.py
+++ cree.py
@@ -6,3 +6,10 @@ df = pd.read_csv('Salary_Data.csv')
 df.head()
 
 
+# splitting the data into the columns which need to be trained(X) and the target column(y)
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+# splitting data into training and testing data with 30 % of data as testing data respectively
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
